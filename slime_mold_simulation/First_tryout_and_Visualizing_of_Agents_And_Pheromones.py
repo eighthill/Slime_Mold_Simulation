d187251fd30582ccc6d5c6668deb83aef3a0f622
Isa <goebel.isabelle@web.de>
2024-01-04 18:57:43 +0100

    added agent class with some functions.

--- slime_mold_simulation/First_tryout_and_Visualizing_of_Agents_And_Pheromones.py
+++ slime_mold_simulation/First_tryout_and_Visualizing_of_Agents_And_Pheromones.py
@@ -1,9 +1,9 @@
-import random
 from random import randint
 
 import matplotlib.pyplot as plt
-import numpy as np
+import math
 
+"""
 # This code is a first try out to identify what classes and methods are needed
 # It is does not work userfriendly without any errors
 # for example if the agents get a random startposition close to the edges it can happen, that the code will raise an error
@@ -43,99 +43,136 @@ class PheromoneArray:
             else:
                 self.array[x, y] = 60
 
-
-# the Parameters here are adjusted but the angle/direction has to be adjusted,
-# right now the code will run an error
+"""
 
 
+# the agent class creates a list with one dictionary for each agent
 class Agent:
-    def __init__(self, pheromone_array, num_agents=10, sensor_angle=15, radius=1):
-        """
-        self.radius = radius
-        self.Agents_list = []
-        self.direction = [1, 1]
-        for idx in range(num_agents):
-            x_pos = randint(0, agent_array.array.shape[0] - 1)
-            y_pos = randint(0, agent_array.array.shape[1] - 1)
-            self.Agents_list.append([x_pos, y_pos, self.direction])
-        """
-
+    def __init__(self, array, num_agents=10, sensor_angle=50, radius=0.2):
         self.num_agents = num_agents
         self.sensor_angle = sensor_angle
-        self.radius = radius
 
-        self.Agents_list = []  # name has been changed
+        # since the radius is now used as distance, maybe the variables name should be changed to distance
+        # if we still want the sensors to look within a given radius we should
+        self.radius = radius
+        self.Agents_list = []
 
         for idx in range(num_agents):
-            x_pos = randint(0, pheromone_array.array.shape[0] - 1)
-            y_pos = randint(0, pheromone_array.array.shape[1] - 1)
+            int_x_pos = randint(0, array.world.shape[0] - 1)
+            int_y_pos = randint(0, array.world.shape[1] - 1)
+            float_x_pos, float_y_pos = self.mapping_int_to_float([int_x_pos, int_y_pos], array)
+            movement_angle = randint(0, 360)  # degree as float?
 
-            # self.direction = [1, 1]  has to be changed to angle instead of list,
-            # here we have a problem now because the direction was safed within the list now it is a list with dictionaryis
-            movement_angle = randint(0, 360)  # degree
-            agent_dict = {"x_pos": x_pos, "y_pos": y_pos, "angle_of_movement": movement_angle}
+            agent_dict = {
+                "int_x_pos": int_x_pos,
+                "int_y_pos": int_y_pos,
+                "float_x_pos": float_x_pos,
+                "float_y_pos": float_y_pos,
+                "movement_angle": movement_angle,
+            }
 
-            # self.Agents_list.append([x_pos, y_pos, self.direction])
             self.Agents_list.append(agent_dict)
 
-    # This method does not work anymore
-    def move_Agents(self, Agents_list, array):
-        for agent in Agents_list:
-            x, y, direction = agent[0], agent[1], agent[2]
+    # this method updates the next move for each agent
+    def make_move(self, array):
+        for agent in self.Agents_list:
+            # the variable next_position contains: [x_coordinate,y_coordinate], angle
+            next_position = self.get_best_move(array, agent)
 
-            # Example: Considering the direction to influence the movement
-            # Here, generate random possible moves
-            possible_moves = [
-                (x + direction[0], y + direction[1]),
-                (x - direction[0], y - direction[1]),
-                (x + direction[1], y - direction[0]),
-                (x - direction[1], y + direction[0]),
-                (x + direction[0], y - direction[0]),
-                (x - direction[0], y + direction[0]),
-                (x + direction[1], y - direction[1]),
-                (x - direction[1], y + direction[1]),
-            ]
+            # the coordinates are given as float
+            agent["float_x_pos"], agent["float_y_pos"] = next_position[0]
 
-            # Filter out moves that are out of bounds
-            possible_moves = [
-                (new_x, new_y)
-                for new_x, new_y in possible_moves
-                if 0 <= new_x < array.shape[0] and 0 <= new_y < array.shape[1]
-            ]
+            # here we have the indicies for the array for each agent,
+            # maybe there is a way to not safe it because we actually have the information already within the float coordinates
+            # and we can always calculate those to integers with the function mapping_float_to_int
+            [agent["int_x_pos"], agent["int_y_pos"]] = self.mapping_float_to_int(next_position[0], array)
 
-            # If no valid moves are possible, do nothing
-            if not possible_moves:
-                continue
+            # this is the direction the agent is looking at after the move
+            agent["movement_angle"] = next_position[1]
 
-            # Randomly select a valid move
-            new_x, new_y = random.choice(possible_moves)
+    # this method compares all possible next positions for an agent to find the best option
+    def get_best_move(self, array, agent):
+        possible_moves = self.get_next_moves(agent)
+        amount_of_pheromones_list = []
 
-            # Update the agent's position
-            agent[0], agent[1] = new_x, new_y
-            """
-            moves = [(x, y), (x - 1, y - 1), (x + 1, y - 1)]
+        # get the amount of pheromones for each possible move and add it to the list amount_of_pheromones_list
+        for move in possible_moves:
+            int_coordinats = self.mapping_float_to_int(move[0], array)
+            value = array.world[int_coordinats[0], int_coordinats[1]]
+            amount_of_pheromones_list.append(value)
 
-            # Find the move with the maximum value in the pheromone array or a random move
-            max_move = random.choice(moves)
+        # find which of the possitions has the most pheremones
+        # here we could implement a method that makes sure that the agents change there way,
+        # if there amount of pheromones has crossed a given value
+        max_val = max(amount_of_pheromones_list)
+        max_idx = amount_of_pheromones_list.index(max_val)
+        next_position = possible_moves[max_idx]
 
-            # Update the agent's position based on the chosen move
-            agent[0], agent[1] = max_move
-            """
+        return next_position
+
+    # calculate new x and y coordinate by given startposition, angle and distance
+    def get_next_moves(self, agent):
+        possible_moves = []
+
+        # here we have 3 sensores, we could add a loop that creates a variable number of sensores
+        angles = [-self.sensor_angle, 0, self.sensor_angle]
+        for angle in angles:
+            angle += agent["movement_angle"]
+            x_new = agent["float_x_pos"] + self.radius * math.cos(angle)
+            y_new = agent["float_y_pos"] + self.radius * math.sin(angle)
+
+            if x_new > 1 or x_new < -1 or y_new > 1 or y_new < -1:
+                # since agents can't leave the array, those lines calulate another angle and coordinates
+                x_new, y_new, angle = self.reflect_at_boundary(x_new, y_new, agent["float_x_pos"], agent["float_y_pos"])
+
+            # this list contains all possible next moves for one agent
+            possible_moves.append([[x_new, y_new], angle])
+        return possible_moves
+
+    # this method is from ChatGPT as a help for now, but it has to be adjusted later in another issue
+    def reflect_at_boundary(self, x, y, x_pos, y_pos):
+        if x > 1:
+            x = 2 - x
+        elif x < -1:
+            x = -2 - x
+        if y > 1:
+            y = 2 - y
+        elif y < -1:
+            y = -2 - y
+        # calculating new angle
+        angle = math.atan2(y - y_pos, x - x_pos)
+        return x, y, angle
+
+    # this method needs a list with 2 float coordinates and calculates them to integer indicies for an given array
+    def mapping_float_to_int(self, coordinates, array):
+        # 2 because of float has to be in [-1,1]
+        float_world_size = 2
+        new_coordinates = []
+
+        for idx, val in enumerate(coordinates):
+            coordinate = int(int((val + 1) / float_world_size * array.world.shape[idx])) - 1
+            new_coordinates.append(coordinate)
+        return new_coordinates
+
+    # this method needs a list with 2 integer indicies for an given array and calculates them to float coordinates
+    def mapping_int_to_float(self, coordinates, array):
+        # 2 because of float has to be in [-1,1]
+        float_world_size = 2
+        new_coordinates = []
+
+        for idx, val in enumerate(coordinates):
+            coordinate = -1 + (float_world_size * val / array.world.shape[idx])
+            new_coordinates.append(coordinate)
+        return new_coordinates
 
 
-# this method move_Agents was almost completly done by ChatGPT
-# here it is important to know that the method does not really find out what move would be the best next move,
-# like where the most pheromones are
-# it just choose random a next move out of 3 options
-# there has to be a method implemented that compares all possible next moves depending on
-# the direction the agent is facing, angle and radius, to decide the next move
-
-
-# This code is only to visualize an given array
+# this method is only to visualize the current array
 def plot_large_array_with_colors(array):
-    plt.imshow(array, interpolation="none", cmap="viridis")
+    plt.imshow(array.world, interpolation="none", cmap="viridis")
     plt.show()
 
+
+"""
 
 # this is the main programm where all the objects are created and methods are called
 def main():
@@ -174,3 +211,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+"""
