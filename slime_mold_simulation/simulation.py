8f3568ab0410e2e96f63c5206c57bd435e9194f7
NielsWulf <NielsWulf@googlemail.com>
2024-02-15 14:35:17 +0100

    Working Logic

--- slime_mold_simulation/simulation.py
+++ slime_mold_simulation/simulation.py
@@ -16,7 +16,7 @@ class SlimeConfig:
     sensor_size = 3
     
 class PheromoneArray:
-    def __init__(self, x_len=1000, y_len=1000, fading=0.97, pheromone_value=10, diffusion_coefficient=0.2):
+    def __init__(self, x_len=1000, y_len=1000, fading=0.97, pheromone_value=0, diffusion_coefficient=0.2):
         self.world = np.zeros((x_len, y_len), dtype=float)
         self.fading = fading
         self.pheromone_value = pheromone_value
@@ -30,35 +30,35 @@ class PheromoneArray:
 
         # Update pheromone concentration using Gaussian filter
         dt = 10  # Time step
-        spread = np.sqrt(4 * self.diffusion_coefficient * dt)
+        spread = np.sqrt(2 * self.diffusion_coefficient * dt)
         self.world = gaussian_filter(self.world, sigma=spread)
 
 
 # the agent class creates a list with one dictionary for each agent
 class Agent:
-    def __init__(self, array, num_agents=1000, sensor_angle=0.33, radius=0.5, speed=0.02):
+    def __init__(self, array, num_agents=1000, sensor_angle=0.33, size=0.02 , radius=0.5, speed=0.02):
         self.num_agents = num_agents
         self.sensor_angle = sensor_angle
         self.radius = radius
         self.speed = speed
         self.Agents_list = []
+        
+        
 
         center_x = array.world.shape[0] // 2
         center_y = array.world.shape[1] // 2
+        angles = np.random.random(num_agents) * 2 * math.pi
+        dst = np.random.random(num_agents) * radius
 
-        # Generate uniformly distributed angles
-        angles = np.linspace(0, 2 * np.pi, num_agents, endpoint=False)
-
-        for angle in angles:
-            # Calculate Cartesian coordinates based on the angle and radius
-            x = center_x + int(center_x * radius * np.cos(angle))
-            y = center_y + int(center_y * radius * np.sin(angle))
+        for angle, d in zip(angles, dst):
+            # Convert polar coordinates to Cartesian coordinates
+            x = center_x + int(d * array.world.shape[0] * np.cos(angle))
+            y = center_y + int(d * array.world.shape[1] * np.sin(angle))
 
             # Ensure the initial position is within the canvas bounds
             x = max(0, min(x, array.world.shape[0] - 1))
             y = max(0, min(y, array.world.shape[1] - 1))
-           # print(f"Initial Position - x: {x}, y: {y}")
-
+            #print(f"Initial Position - x: {x}, y: {y}")
             float_x_pos, float_y_pos = self.mapping_int_to_float([x, y], array)
             movement_angle = np.random.uniform(0, 2 * np.pi)
 
@@ -72,7 +72,8 @@ class Agent:
             }
 
             self.Agents_list.append(agent_dict)
-
+            
+            
     # this method updates the next move for each agent
     def make_move(self, array):
         for agent in self.Agents_list:
@@ -89,7 +90,7 @@ class Agent:
 
             # Update the direction the agent is looking at after the move
             agent["movement_angle"] = next_position[1] + randint(-5, 5)
-            print(f"Updated Position - x: {agent['float_x_pos']}, y: {agent['float_y_pos']}")
+           # print(f"Updated Position - x: {agent['float_x_pos']}, y: {agent['float_y_pos']}")
 
     def get_best_move(self, array, agent):
         possible_moves = self.get_next_moves(agent)
@@ -123,40 +124,43 @@ class Agent:
         return possible_moves
 
     def reflect_at_boundary(self, x, y, x_pos, y_pos):
-        # Reflect at the boundary
-        x = 2 * (x > 1) - x
-        y = 2 * (y > 1) - y
-        x = 2 * (x < 0) - x
-        y = 2 * (y < 0) - y
-        print(f"Reflected Position - x: {x}, y: {y}")
-        # Calculate new angle
+        # check if absolute value is greater than 1
+        # if true, reflect point from subtracting *2 the result of a comparison (if statements)
+        if abs(x) > 1:
+            x = 2 * (x > 0) - x
+
+        if abs(y) > 1:
+            y = 2 * (y > 0) - y
+
         dx = x - x_pos
         dy = y - y_pos
+
+        # Calculate new angle
         angle = math.atan2(dy, dx)
 
         return x, y, angle
 
-    # this method needs a list with 2 float coordinates and calculates them to integer indicies for an given array
+    # this method needs a list with 2 float coordinates and calculates them to integer indices for a given array
     def mapping_float_to_int(self, coordinates, array):
         # 2 because of float has to be in [-1,1]
         float_world_size = 2
         new_coordinates = []
 
         for idx, val in enumerate(coordinates):
-            coordinate = int((val + 1) / float_world_size * array.world.shape[idx])
+            coordinate = int((val + 1) * 0.5 * array.world.shape[idx])
 
             # ensure coordinate is within bounds
             coordinate = max(0, min(array.world.shape[idx] - 1, coordinate))
             new_coordinates.append(coordinate)
         return new_coordinates
 
-    # this method needs a list with 2 integer indicies for an given array and calculates them to float coordinates
+    # this method needs a list with 2 integer indices for a given array and calculates them to float coordinates
     def mapping_int_to_float(self, coordinates, array):
         # 2 because of float has to be in [-1,1]
         float_world_size = 2
         new_coordinates = []
 
         for idx, val in enumerate(coordinates):
-            coordinate = -1 + (float_world_size * val / array.world.shape[idx])
+            coordinate = -1 + (2 * val / array.world.shape[idx])
             new_coordinates.append(coordinate)
         return new_coordinates
