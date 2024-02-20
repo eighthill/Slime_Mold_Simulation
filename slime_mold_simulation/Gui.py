101db59b84f3aedaa4d607747080143cb84596a5
ModusMorris <maurice.191999@gmail.com>
2024-02-20 23:07:37 +0100

    changed agent position equal to pheromon position, not mirrored anymore

--- slime_mold_simulation/Gui.py
+++ slime_mold_simulation/Gui.py
@@ -26,27 +26,34 @@ class SimulationGUI(app.Canvas):
         app.Canvas.__init__(
             self,
         )
-
         # Set up a timer for periodic updates
+
         self.timer = app.Timer(connect=self.on_timer, start=True)
         # Initialize the PheromoneArray and Agent instances
         p_array = PheromoneArray()
         agneten = Agent()
         self.parray = p_array.p_array
         self.agnet = agneten.agenten
+
         self.view = scene.SceneCanvas(keys="interactive", size=(HEIGHT, WIDTH), show=True)
         self.view.events.draw.connect(self.on_draw)
+        # Create a visual representing the agents
+        self.agents = scene.visuals.Markers(pos=self.agnet[:, :2], face_color='white', symbol='o', size=100, parent=self.view.scene)
+
         # Create an image visual representing the pheromone array
         self.image = scene.visuals.Image(self.parray, cmap="viridis", parent=self.view.scene)
-
+        
     def on_draw(self, event):
         """
         Event handler for drawing on the canvas.
         Updates the visual representation of the pheromone array.
         """
-
         # Set the data of the image visual to the current pheromone array
         self.image.set_data(self.parray)
+        # Set the position of the agent visual to the current position of the agents
+        self.agnet[:, [0, 1]] = self.agnet[:, [1, 0]]
+        self.agents.set_data(pos=self.agnet[:, :2])
+        self.agnet[:, [1, 0]] = self.agnet[:, [0, 1]]
 
     def on_timer(self, event):
         """
@@ -54,9 +61,8 @@ class SimulationGUI(app.Canvas):
         Updates the pheromone array and agent movements periodically.
         """
         self.parray, self.agnet = main(self.parray, self.agnet)
-        # Update the Vispy scene to reflect the changes
+        self.agents.set_data(pos=self.agnet[:, :2])
         self.view.scene.update()
-
 
 if __name__ == "__main__":
     # Main Program
