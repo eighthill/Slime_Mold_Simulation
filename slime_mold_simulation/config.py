6a68f63dd11c77fdf93714ee97684c9fee07b030
NielsWulf <NielsWulf@googlemail.com>
2024-02-21 18:18:25 +0100

    Gui update + Slider

--- slime_mold_simulation/config.py
+++ slime_mold_simulation/config.py
@@ -1,5 +1,4 @@
 # This is the Configutaion file with all global parameters
-<<<<<<< HEAD
 import numpy as np
 
 
@@ -27,20 +26,3 @@ class SlimeConfig:
     @classmethod
     def set_agent_count(cls, new_agent_count):
         cls.AGENT_NUMBER = new_agent_count
-=======
-
-# Simulationparameters
-WIDTH = 1000
-HEIGHT = 1000
-DECAY = 0.97
-DIFFUSION_COEFFICENT = 0.5
-
-
-AGENT_NUMBER = 100
-SPEED = 1
-SENSOR_DISTANCE = 3
-ROTATION_SPEED = 0.2
-# the angles are calculated with radian (0 = 0° and 2*pi = 360°) in the simulation Code so the given angle shoulr be between 0 and 2pi.
-# here the formula X/180*pi is calculating a given degree X into a radian
-SENSOR_ANGLE = 33
->>>>>>> 5f2f9ffff8518091fa2f1e6395485042b4a1b27e
