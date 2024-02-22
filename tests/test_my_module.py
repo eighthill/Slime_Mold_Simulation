41ffde8f2ef2382070bf71e48a0b2da76708dfd6
Krabgalasch <krabgalasch@gmail.com>
2024-02-22 14:16:50 +0100

    prepared for testing

--- tests/test_my_module.py
+++ tests/test_my_module.py
@@ -1,6 +1,27 @@
 """Tests for the slime_mold_simulation.my_module module."""
 
 # import numpy as np
-# import pytest
+import pytest
 
-# from slime_mold_simulation.simulation import diffuse, decay, get_sensors, get_pheromone_value_at, reflect_boundary, move, deposit_pheromone, rotate_towards_sensor
+from slime_mold_simulation.my_module import hello
+
+# from slime_mold_simulation.simulation import get_sensors
+
+
+def test_hello():
+    assert hello("Alice") == "Hello Alice!"
+
+
+def test_hello_with_error():
+    with pytest.raises(ValueError) as excinfo:
+        hello("nobody")
+    assert "Can not say hello to nobody" in str(excinfo.value)
+
+
+@pytest.fixture
+def some_name():
+    return "Jane Smith"
+
+
+def test_hello_with_fixture(some_name):
+    assert hello(some_name) == "Hello Jane Smith!"
